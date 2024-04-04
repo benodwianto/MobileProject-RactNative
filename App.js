b04ff169eca0c197f53865c2eea43196af643674
Beno Dwianto <benodwianto9@gmail.com>
2024-04-04 15:50:14 +0700

    menambahkan style

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { styles } from './Style'; // Mengimpor gaya dari file Style.js
 
 export default function App() {
-  return (
+  return ( 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Must a nice,Must a Nice</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'green',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export { styles };
